1b1a26e14292895037f9e90646d2fdb28a336a22
Jeff Rothkirch <jeff.rothkirch@gmail.com>
2015-12-23 00:26:44 -0500

    added unit test

---
+++ AbcBank.Test/AccountTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AbcBank.Test
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        [Test]
+        public void checking_InterestIearned_Correct()
+        {
+            double depoistAmount = 5000000;
+            double expectedInterest = (depoistAmount * 0.001);
+
+            var account = new Account(Account.CHECKING);
+            account.deposit(depoistAmount);
+
+            Assert.AreEqual(expectedInterest, account.interestEarned());
+        }
+
+        [Test]
+        public void savings_InterestIearned_Correct()
+        {
+            var account = new Account(Account.SAVINGS);
+            account.deposit(5000000);
+
+            Assert.AreEqual(9999.0, account.interestEarned());
+        }
+
+        [Test]
+        public void maxisavings_InterestIearned_NoTransactionsTenDays_Correct()
+        {
+            var account = new Account(Account.MAXI_SAVINGS);
+            account.deposit(5000000);
+
+            Assert.AreEqual(5000000 * 0.05, account.interestEarned());
+        }
+    }
+}
